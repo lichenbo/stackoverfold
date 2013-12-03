cad1f49044dba98215c4279fa19113c050eab859
Chenbo Li <lichenbo1949@gmail.com>
2013-12-03 21:46:46 +0800

    run

--- testNode/blog/routes/index.js
+++ testNode/blog/routes/index.js
@@ -5,8 +5,14 @@
 var ArticleProvider = require('../articleprovider-memory.js').ArticleProvider;
 exports.index = function(req, res){
   //res.render('index', { title: 'Express' });
-   var articleProvider = new ArticleProvider();
+  var articleProvider = new ArticleProvider();
   articleProvider.findAll(function(error,docs){
-      res.send(docs)
+      console.log(docs)
+      res.render('index.jade',
+      {
+          title: 'Blog',
+          articles:docs
+      }
+      );
   });
-};
+}
